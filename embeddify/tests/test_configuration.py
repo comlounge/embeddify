7eb4c9b279c064bb4c6370f4fc8112ac6825d8fa
Florian Schulze <mail@florian-schulze.net>
2025-06-17 14:14:38 +0200

    Adjust a bunch of tests to changed results.

--- embeddify/tests/test_configuration.py
+++ embeddify/tests/test_configuration.py
@@ -1,12 +1,19 @@
 from embeddify import Embedder, YouTube
 
+
 def test_generic_youtube():
     embedder = Embedder(width=200)
-    assert embedder("https://www.youtube.com/watch?v=2wii8hfNkzE") == """<iframe width="200" height="113" src="https://www.youtube.com/embed/2wii8hfNkzE?feature=oembed" frameborder="0" allowfullscreen></iframe>"""
+    result = embedder("https://www.youtube.com/watch?v=2wii8hfNkzE")
+    assert "<iframe" in result
+    assert "https://www.youtube.com/embed/2wii8hfNkzE" in result
+    assert 'height="113"' in result
+    assert 'width="200"' in result
+
 
 def test_generic_slideshare():
     embedder = Embedder(width=200)
     assert embedder("http://de.slideshare.net/mrtopf/open-government-vortrag-aachen").startswith("""<iframe src="https://www.slideshare.net/slideshow/embed_code/key/3wGhGoN6QUw5lR" """)
+
 
 def test_plugin_specific_config():
     cfg = {
@@ -18,28 +25,42 @@ def test_plugin_specific_config():
 
     assert embedder("http://de.slideshare.net/mrtopf/open-government-vortrag-aachen").startswith("""<iframe src="https://www.slideshare.net/slideshow/embed_code/key/3wGhGoN6QUw5lR" width="427" height="356" """)
 
+
 def test_plugin_selection():
     """only use youtube"""
     embedder = Embedder(plugins = [YouTube()])
-    assert embedder("https://www.youtube.com/watch?v=2wii8hfNkzE") == """<iframe width="560" height="315" src="https://www.youtube.com/embed/2wii8hfNkzE?feature=oembed" frameborder="0" allowfullscreen></iframe>"""
+    result = embedder("https://www.youtube.com/watch?v=2wii8hfNkzE")
+    assert "<iframe" in result
+    assert "https://www.youtube.com/embed/2wii8hfNkzE" in result
     assert embedder("http://de.slideshare.net/mrtopf/open-government-vortrag-aachen") == "http://de.slideshare.net/mrtopf/open-government-vortrag-aachen"
+
 
 def test_by_call_configuration():
     embedder = Embedder(width=400)
     assert embedder.plugin_config['youtube'] == dict(width=400)
-    assert embedder("https://www.youtube.com/watch?v=2wii8hfNkzE", width=200) == """<iframe width="200" height="113" src="https://www.youtube.com/embed/2wii8hfNkzE?feature=oembed" frameborder="0" allowfullscreen></iframe>"""
+    result = embedder("https://www.youtube.com/watch?v=2wii8hfNkzE", width=200)
+    assert "<iframe" in result
+    assert "https://www.youtube.com/embed/2wii8hfNkzE" in result
+    assert 'height="113"' in result
+    assert 'width="200"' in result
     assert embedder.plugin_config['youtube'] == dict(width=400)
 
 
 def test_autoplay():
     embedder = Embedder(autoplay=True)
     result = embedder("http://vimeo.com/6791752")
-    assert result.startswith("""<iframe src="https://player.vimeo.com/video/6791752?autoplay=1" width="420" height="315" """)
+    assert "<iframe" in result
+    assert "https://player.vimeo.com/video/6791752" in result
+    assert "autoplay=1" in result
     result = embedder("https://www.youtube.com/watch?v=SC93q4tZeNI")
-    assert result == """<iframe width="560" height="315" src="https://www.youtube.com/embed/SC93q4tZeNI?feature=oembed&autoplay=1" frameborder="0" allowfullscreen></iframe>"""
+    assert "<iframe" in result
+    assert "https://www.youtube.com/embed/SC93q4tZeNI" in result
+    assert "autoplay=1" in result
 
 
 def test_params():
     embedder = Embedder()
     result = embedder("http://vimeo.com/6791752", params=dict(autoplay=True))
-    assert result.startswith("""<iframe src="https://player.vimeo.com/video/6791752?autoplay=1" width="420" height="315" """)
+    assert "<iframe" in result
+    assert "https://player.vimeo.com/video/6791752" in result
+    assert "autoplay=1" in result
--- embeddify/tests/test_generic.py
+++ embeddify/tests/test_generic.py
@@ -1,37 +1,49 @@
 
 def test_youtube(embedder):
     result = embedder("https://www.youtube.com/watch?v=SC93q4tZeNI")
-    assert result == """<iframe width="560" height="315" src="https://www.youtube.com/embed/SC93q4tZeNI?feature=oembed" frameborder="0" allowfullscreen></iframe>"""
+    assert '<iframe' in result
+    assert 'src="https://www.youtube.com/embed/SC93q4tZeNI' in result
+    assert 'width="560"' in result
+    assert 'height="315"' in result
     assert result.data['thumbnail_url'] == u'https://i.ytimg.com/vi/SC93q4tZeNI/hqdefault.jpg'
     assert isinstance(result, embedder.OEmbedMarkup)
     result = embedder("https://youtu.be/SC93q4tZeNI")
-    assert result == """<iframe width="560" height="315" src="https://www.youtube.com/embed/SC93q4tZeNI?feature=oembed" frameborder="0" allowfullscreen></iframe>"""
+    assert '<iframe' in result
+    assert 'src="https://www.youtube.com/embed/SC93q4tZeNI' in result
+    assert 'width="560"' in result
+    assert 'height="315"' in result
     assert result.data['thumbnail_url'] == u'https://i.ytimg.com/vi/SC93q4tZeNI/hqdefault.jpg'
     assert isinstance(result, embedder.OEmbedMarkup)
 
 
 def test_slideshare(embedder):
     result = embedder("http://de.slideshare.net/mrtopf/open-government-vortrag-aachen")
-    assert result.startswith("""<iframe src="https://www.slideshare.net/slideshow/embed_code/key/3wGhGoN6QUw5lR" width="427" height="356" """)
-    assert result.data['thumbnail_url'] == u'https://cdn.slidesharecdn.com/ss_thumbnails/opengov-aachen-120619124815-phpapp02-thumbnail.jpg?cb=1340110538'
+    assert '<iframe' in result
+    assert 'src="https://www.slideshare.net/slideshow/embed_code/key/3wGhGoN6QUw5lR' in result
+    assert 'width="427"' in result
+    assert 'height="356"' in result
+    assert result.data['thumbnail_url'].startswith(u'https://cdn.slidesharecdn.com/ss_thumbnails/opengov-aachen-120619124815-phpapp02-thumbnail.jpg')
     assert isinstance(result, embedder.OEmbedMarkup)
 
 
 def test_flickr(embedder):
     result = embedder("http://www.flickr.com/photos/mrtopf/7780673842/")
-    assert result == """<a target="flickr" href="https://www.flickr.com/photos/mrtopf/7780673842/"><img src="https://farm9.staticflickr.com/8297/7780673842_a57b60cd16_n.jpg" class="flickr-embed-img" alt="professionelle politische Kommunikation" width="320" height="250"></a>"""
-    assert result.data['thumbnail_url'] == u'https://farm9.staticflickr.com/8297/7780673842_a57b60cd16_q.jpg'
+    assert result == """<a target="flickr" href="https://www.flickr.com/photos/mrtopf/7780673842/"><img src="https://live.staticflickr.com/8297/7780673842_a57b60cd16_n.jpg" class="flickr-embed-img" alt="professionelle politische Kommunikation" width="320" height="250"></a>"""
+    assert result.data['thumbnail_url'] == u'https://live.staticflickr.com/8297/7780673842_a57b60cd16_q.jpg'
     assert isinstance(result, embedder.OEmbedMarkup)
 
 
 def test_vimeo(embedder):
     result = embedder("http://vimeo.com/6791752")
-    assert result.startswith("""<iframe src="https://player.vimeo.com/video/6791752" width="420" height="315" """)
-    assert result.data['thumbnail_url'] == u'https://i.vimeocdn.com/video/27011558_295x166.jpg'
+    assert 'src="https://player.vimeo.com/video/6791752' in result
+    assert 'width="420"' in result
+    assert 'height="315"' in result
+    assert u'https://i.vimeocdn.com/video/27011558' in result.data['thumbnail_url']
+    assert u'295x166' in result.data['thumbnail_url']
     assert isinstance(result, embedder.OEmbedMarkup)
 
 
-def test_facebook(embedder):
+def skip_facebook(embedder):  # disabled the test, because an access token is required
     result = embedder("https://www.facebook.com/facebook/videos/10153231379946729/")
     assert 'data-href="https://www.facebook.com/facebook/videos/10153231379946729/"' in result
     assert 'thumbnail_url' not in result.data
